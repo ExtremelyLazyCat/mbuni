212d866714b3b081afe287d3eef8566c28aa281f
Jack <jackleea1b@gmail.com>
2025-12-19 08:58:09 -0700

    Fix IP->MSISDN issue for phones connecting through WAP

--- mmsc/mmsproxy.c
+++ mmsc/mmsproxy.c
@@ -92,12 +92,30 @@ static void mm1proxy(void)
 	       if (!h.base_client_addr) { /* Set to IP sender... XXXX assumes ipv4 only for now*/
 		    if (settings->allow_ip_type) {
 			int ipv6 = 0;
-			h.base_client_addr = mms_find_sender_ip(h.headers,
-								settings->wap_gw_ip_header, 
-								h.ip, &ipv6);
-			h.client_addr  = octstr_format("%S/TYPE=IPv%s", 
-						       h.base_client_addr, 
-						       ipv6 ? "6" : "4");
+                        Octstr* sender_ip = mms_find_sender_ip(h.headers,
+                                                              settings->wap_gw_ip_header,
+                                                              h.ip, &ipv6);
+
+                        /* Retry receiving sender address with WAP IP Header */
+                        Octstr* addr_from_sender_ip = mms_find_sender_msisdn(h.url,
+                                                                            sender_ip,
+                                                                            h.headers,
+                                                                            settings->wap_gw_msisdn_header,
+                                                                            settings->wap_gw_ip_header,
+                                                                            settings->mms_detokenizefuncs);
+
+                        if (addr_from_sender_ip) {
+                                h.base_client_addr = addr_from_sender_ip;
+                                goto msisdnFixup;
+                        }
+
+                        else {
+                                h.base_client_addr = sender_ip;
+                                h.client_addr  = octstr_format("%S/TYPE=IPv%s",
+                                                               h.base_client_addr,
+                                                               ipv6 ? "6" : "4");
+                        }
+
 		    } else 
 		         h.client_addr = NULL;
 		    
@@ -109,6 +127,7 @@ static void mm1proxy(void)
 		    else 
 			 h.client_addr = octstr_duplicate(h.base_client_addr);
 	       } else  { /* A bare number, normalise it. */
+msisdnFixup:
 		    _mms_fixup_address(&h.base_client_addr, 
 				       octstr_get_cstr(settings->unified_prefix), 
 				       settings->strip_prefixes, 0);		    
